0118b37b8270715177db4c1c52f606405aa4d8f2
Daiki Shimizu <mildlight28@gmail.com>
2025-09-25 03:32:20 +0900

    Initial Commit

---
+++ HelloStack/HelloStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloStackApp.swift
+//  HelloStack
+//
+//  Created by 清水大喜 on 2025/09/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
